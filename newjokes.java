24350ed4306622fa6f1feecc9ae4ec9074b0055e
VisaliniSS <visalini864@gmail.com>
2025-10-28 15:49:37 +0530

    Add files via upload

---
+++ newjokes.java
@@ -0,0 +1,41 @@
+package NewJokes;
+
+import java.util.*;
+
+class NewJokes {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        String j=sc.nextLine();
+
+        System.out.printl("Enter the version of your jokes");
+
+        String k=sc.nextLine();
+
+        System.out.println("Your Jokes are here");
+
+        for(int i=0;i<n;i++){
+
+            for(int j=0;j<n;j++){
+
+                if(i=="old"){
+
+                    System.out.println("Generate new Jokes");
+
+                }
+
+                else if(i=="new"){
+
+                    System.out.println("Generate old jokes");
+                }
+
+
+
+            }
+        }
+
+
+
+}
